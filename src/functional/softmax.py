Fixing softmax, adding cross_entropy and casual attention

src/functional/softmax.py
@@ -2,6 +2,7 @@ import torch
 
 
 def softmax(x: torch.Tensor, dim: int = -1) -> torch.Tensor:
-    x1 = torch.exp(x)
-    s = torch.sum(x1)
+    max_x = x.max(dim, keepdim=True).values
+    x1 = torch.exp(x-max_x)
+    s = torch.sum(x1, dim=dim, keepdim=True)
     return x1.div(s)